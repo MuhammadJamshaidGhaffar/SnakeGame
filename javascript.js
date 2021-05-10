c9c46de346b37185ef7c8fa5dbcd45bce8fbadfc
MuhammadJamshaidGhaffar <muhammadjamshaid89@gmail.com>
2021-05-10 10:10:17 +0500

    Adding game over

--- javascript.js
+++ javascript.js
@@ -8,6 +8,7 @@ let Apple = {x : 0 , y:0 };
 //--------- Game States ---------------
 let snakeMovement = "right";
 let Score = 0;
+let gameStatus = "Not Running";
 
 //-------- conatiner------------
 let width = 50;
@@ -18,24 +19,44 @@ let finalHeight = boxSize * height;
 
 let gameStateInfoHeight = 30;
 
+//----------- Setting up width and height ------------------
+document.getElementById("game-state-info").style.width = finalWidth+"px";
+document.getElementById("game-state-info").style.height= gameStateInfoHeight+"px";
+
+document.getElementById("container").style.width = finalWidth+"px";
+document.getElementById("container").style.height= finalHeight+"px";
+
+document.getElementById("snake").style.width = boxSize+"px";
+document.getElementById("snake").style.height= boxSize+"px";
+
+document.getElementById("apple").style.width = boxSize+"px";
+document.getElementById("apple").style.height= boxSize+"px";
 
 //--------- Utility Functions ------------
-function isCollide(object1 , object2){
+function isCollided(object1 , object2){
   if(object1.x == object2.x && object1.y == object2.y)
     return true;
   return false;
 }
-function isHeadCollide(position){
-  if(isCollide(Snake.head , position))
+function isHeadCollided(position){
+  if(isCollided(Snake.head , position))
     return true;
   return false;
 }
-function isAppleCollideWithBody(){
-  if(isCollide(Snake.head , Apple))
+function isHeadCollidedWithBody()
+{
+  for(let bodyDot = 0  ; bodyDot < Snake.body.length ; bodyDot++){
+    if(isHeadCollided(Snake.body[bodyDot]))
+      return true;
+  }
+  return false;
+}
+function isAppleCollidedWithBody(){
+  if(isCollided(Snake.head , Apple))
     return true;
   for(let bodyDot =0 ; bodyDot <  Snake.body.length ; bodyDot++)
   {
-    if(isCollide(bodyDot , Apple))
+    if(isCollided(bodyDot , Apple))
       return true;
   }
   return false;
@@ -43,27 +64,8 @@ function isAppleCollideWithBody(){
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
-//------ Spawn Functions --------------
-function spawnApple()
-{
-  do{
-    Apple.x = getRndInteger(0 , width);
-    Apple.y = getRndInteger(0 , height);
-  }
-  while(isAppleCollideWithBody());
-  Apple.x *= boxSize;
-  Apple.y *= boxSize;
-  document.getElementById("apple").style.transform = 'translate(' +Apple.x +'px ,'+Apple.y +'px )';
-}
-function addBodyDot()
-{
-  let snakeBody = document.createElement('div');
-  snakeBody.className = "snakeBody";
-  document.getElementById('container').appendChild(snakeBody);
-  Snake.body.push({x:0 , y:0});
-}
-//-------- update functions --------------
-function moveSnake(event){
+//------- Input Detection Functions --------------
+function changeSnakeDirection(event){
   //------ bug ----
   //-- if we press two keys at the same time then snake can move opposite to its current direction
     if(event.key == "ArrowDown")
@@ -90,9 +92,57 @@ function moveSnake(event){
         return;
       snakeMovement = "right";
     }
-    // alert(event.key);
-    // alert(translateStr);
+      
   }
+function gameNotRunningKeysDetection(event)
+{
+  if(event.key == "Enter")
+  {
+    gameRestart();
+    
+  }
+}
+//------ Spawn Functions --------------
+function spawnApple()
+{
+  do{
+    Apple.x = getRndInteger(0 , width);
+    Apple.y = getRndInteger(0 , height);
+  }
+  while(isAppleCollidedWithBody());
+  Apple.x *= boxSize;
+  Apple.y *= boxSize;
+  document.getElementById("apple").style.transform = 'translate(' +Apple.x +'px ,'+Apple.y +'px )';
+}
+function addBodyDot()
+{
+  let snakeBody = document.createElement('div');
+  snakeBody.className = "snakeBody";
+  document.getElementById('container').appendChild(snakeBody);
+  Snake.body.push({x:0 , y:0});
+}
+function removeBody()
+{
+  let snakeBody =  document.getElementsByClassName('snakeBody');
+  for(let i=0 ; i<snakeBody.length ; i++)
+  {
+    snakeBody[i].parentNode.removeChild(snakeBody[i]);
+  }
+  alert("removed Body");
+}
+
+//-------- update functions --------------
+
+function updateBody(snakeOldHead)
+{
+  translateBody(snakeOldHead);
+  printBody();
+}
+function updateScore()
+{
+  document.getElementById("score-value").innerHTML = Score;
+}
+//---------- Movement Functions----------------
   function translateSnake()
 {
   if(snakeMovement == "right")
@@ -127,9 +177,8 @@ function moveSnake(event){
       Snake.head.y = 0;
     }
   }
-document.getElementById('snake').style.transform = 'translate(' +Snake.head.x +'px ,'+Snake.head.y +'px )';
 }
-function moveBody(snakeOldHead)
+function translateBody(snakeOldHead)
 {
   for(let bodyDot = Snake.body.length-1 ; bodyDot >  0 ; bodyDot--)
   {
@@ -139,82 +188,95 @@ function moveBody(snakeOldHead)
   Snake.body[0].x = snakeOldHead.x;
   Snake.body[0].y = snakeOldHead.y;
 }
+//------------- Print Functions -----------------
+function printSnakeHead()
+{
+  document.getElementById('snake').style.transform = 'translate(' +Snake.head.x +'px ,'+Snake.head.y +'px )';
+}
+
 function printBody()
 {
   let snakeBody = document.getElementsByClassName("snakeBody");
   for(let bodyDot =0 ; bodyDot < snakeBody.length  ; bodyDot++)
   {
-    console.log("bodyDot["+bodyDot+"].x = " + Snake .body[bodyDot].x);
     snakeBody[bodyDot].style.transform = 'translate(' +Snake.body[bodyDot].x +'px ,'+Snake.body[bodyDot].y +'px )';
 
   }
 }
-function updateBody(snakeOldHead)
-{
-  moveBody(snakeOldHead);
+function printSnake(){
+  printSnakeHead();
   printBody();
 }
-function updateScore()
-{
-  document.getElementById("score-value").innerHTML = Score;
-}
 
-
-
-//--------------- Game Start --------------
-document.getElementById("game-state-info").style.width = finalWidth+"px";
-document.getElementById("game-state-info").style.height= gameStateInfoHeight+"px";
-
-document.getElementById("container").style.width = finalWidth+"px";
-document.getElementById("container").style.height= finalHeight+"px";
-
-document.getElementById("snake").style.width = boxSize+"px";
-document.getElementById("snake").style.height= boxSize+"px";
-
-document.getElementById("apple").style.width = boxSize+"px";
-document.getElementById("apple").style.height= boxSize+"px";
-
-spawnApple();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-addBodyDot();
-
-
-//---------- starting Game Loop ---------------
-document.addEventListener("keydown", moveSnake);
-setInterval(gameLoop , 100);
-
-//--------------- Game Loop ------------------
+//------------- Game Logic---------------
 function gameLoop()
 {
+  //---------- Moving Snake ---------------
   let snakeOldHead = {x : Snake.head.x  , y : Snake.head.y} ;
   translateSnake();
-  updateBody(snakeOldHead);
-  if(isHeadCollide(Apple))
+  if(isHeadCollided(Apple))
   {
     spawnApple();
-    addBodyDot();
     Score++;
     updateScore();
-    
+    addBodyDot();
   }
-  
+  else if(isHeadCollidedWithBody())
+  {
+    
+    gameOver();
+    return;
+  }
+  // ---------- Printing --------------
+  printSnakeHead();
+  updateBody(snakeOldHead);
 }
 
+function gameOver()
+{
+  clearInterval(threadId);
+  gameStatus = "Not Running";
+  document.getElementById('game-over-text').style.opacity = "1";
+  document.removeEventListener("keydown", changeSnakeDirection);
+  document.addEventListener("keydown", gameNotRunningKeysDetection);
+}
+function gameRestart()
+{
+  //----------- Removing GameOver Text ----------------
+  document.getElementById('game-over-text').style.opacity = "0";
+  // ----- seeting up listeners ---------------
+  document.removeEventListener("keydown", gameNotRunningKeysDetection);
+  document.addEventListener("keydown", changeSnakeDirection);
+  //------- Resetting Variables-------------
+  Snake = {head : {x:0 , y:0} , body : []};
+  Apple = {x : 0 , y:0 };
+  snakeMovement = "right";
+  Score = 0;
+  //--------- Deleting Spawned Objects ---------------------
+  removeBody();
+  //----------- Running Game Again --------------
+  main();
+      
+}
+
+function main()
+{
+  //--------------
+  spawnApple();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  addBodyDot();
+  //---------- starting Game Loop ---------------
+  gameStatus = "Running";
+  document.addEventListener("keydown", changeSnakeDirection);
+  threadId =  setInterval(gameLoop , 100);
+}
+
+main();
   
